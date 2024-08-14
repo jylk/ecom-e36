e110389c483397502ec19000bbc23f0b85c2a1f3
Jaya lakshmi <rjayalakshmi901@gmail.com>
2024-08-14 10:24:39 +0530

    first commit

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Col, Container, ListGroup, Row } from 'react-bootstrap'
-import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom';
 
 const ProductDetails = () => {
 
